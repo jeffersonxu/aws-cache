0232be7511e065357ed8c01c7ccda6d8e1c70e0f
jeffersonxu <me@jeffersonxu.com>
2018-08-20 14:04:21 -0400

    Cleanup code

--- cache.js
+++ cache.js
@@ -5,25 +5,31 @@ class Cache{
 	constructor(port, host, endpoint){
 		this.endpoint = endpoint;
 		this.client = redis.createClient(port, host);
-	}
-	connected(){
 		this.client.on("error", function(err){
 			console.log(err);
 		});
 		this.client.on("connect", function () {
-		  console.log("Connected: ", this.client);
+			console.log("Connected to " host + ":" + port + "\n");
 		});
+	}
+	connected(){
+		return this.client.connected;
 	}
 	set(key, value){
 		this.client.set(key, value, redis.print);
+		console.log(key + " successfully set to " + value);
 	}
 	get(key){
 		this.client.get(key, function(err, reply) {
 			if (err) {
 			    console.log(err.toString());
 			} else {
-			    console.log('Reply: ' + reply);
-			    return reply;
+				if(reply == null){
+					console.log(key + " doesn't exist. Proceed with GET");
+					//data = GET
+					//this.set(key, data);
+				}
+			    console.log(reply);
 			}
 		});
 	}
--- test.js
+++ test.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const Cache = require('./cache.js');
 
-let test = new Cache(6379, 'blacklapel-test.0f6gsj.ng.0001.use2.cache.amazonaws.com', 'test');
+//Insert port, host, and endpoint here
+let redis = new Cache(6379, 'blacklapel-test.0f6gsj.ng.0001.use2.cache.amazonaws.com', 'test');
+
 const app = express();
 app.get('/*', (req, res) => {
-	res.send('Hello world\n');
-	console.log(req.params[0]);
-
-	test.connected();
-	test.set("test key", "test value");
-	let data = test.get("test key");
-	if(data === null){
-		console.log('wow it be null');
+	if(req.params[0] !== 'favicon.ico'){
+		redis.set(req.params[0], req.params[0] + " test data");
+		redis.get(req.params[0]);
 	}
-	else{
-		console.log(data);
-	}
+	res.send("Testing bananas");
 });
 
 app.listen(PORT, HOST);
